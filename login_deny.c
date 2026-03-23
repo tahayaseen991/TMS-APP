d8e6c1f9e603416dcaccd22d67a91a68b0df38c2
Taha Yaseen <taha.yaseen991@gmail.com>
2026-03-24 00:01:53 +0500

    Update login_deny.c

--- login_deny.c
+++ login_deny.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// Function called when login fails
 void login_deny() {
     printf("\nAccess Denied!\n");
+
+    // Pause until user presses enter
+    printf("Press ENTER to return to login screen...");
+    getchar(); // consume previous newline
+    getchar(); // wait for user input
 }
