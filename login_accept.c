5d0113a70a4cf066ff8f24a2463c9c01ddf1da91
Taha Yaseen <taha.yaseen991@gmail.com>
2026-03-24 00:01:34 +0500

    Update login_accept.c

--- login_accept.c
+++ login_accept.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// Function called when login is successful
 void login_accept() {
-    printf("\nWelcome to TMS System!\n");
+    printf("\nLogin Successful!\n");
+
+    // Pause until user presses enter
+    printf("Press ENTER to continue to Main Menu...");
+    getchar(); // consume previous newline
+    getchar(); // wait for user input
+
+    // Call main menu
+    extern void main_menu(); // main_menu defined in main_menu.c
+    main_menu();
 }
