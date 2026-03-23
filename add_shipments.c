85a52b99c2343fb95b0e1fa70428c4a2d396f479
Taha Yaseen <taha.yaseen991@gmail.com>
2026-03-24 01:28:06 +0500

    Add New Shipments Module
    Add new shipments

---
+++ add_shipments.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void add_shipments() {
+    printf("\n--- ADD SHIPMENTS MODULE ---\n");
+    printf("Functionality to add a new shipment will go here.\n");
+}
