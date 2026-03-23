c4c95b2ef624101e22c214d8b0303183818beaa8
Taha Yaseen <taha.yaseen991@gmail.com>
2026-03-24 01:26:19 +0500

    View All Shipments Module
    View all shipments

---
+++ view_shipments.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void view_shipments() {
+    printf("\n--- VIEW SHIPMENTS MODULE ---\n");
+    printf("Functionality to display all shipments will go here.\n");
+}
